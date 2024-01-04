392359e7dbcaa52a38b01ec45ca0ec2bf59956de
imxela <65206220+imxela@users.noreply.github.com>
2024-01-04 15:45:15 +0100

    ci: try to fix broken styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://josh-hartop.github.io/',
-  base: './'
+  base: '/website'
 });
