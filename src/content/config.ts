9e37ea6dc5c2a63e396c8e0b99870aba4d76d827
imxela <65206220+imxela@users.noreply.github.com>
2024-01-19 14:43:07 +0100

    feat: add project card download button & redesign card buttons

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,6 +7,11 @@ const projectsCollection = defineCollection({
         tags: z.array(z.string()),
         description: z.string(),
         github_url: z.string().url().optional(),
+
+        // Todo: could probably query this information using GitHub's API but this'll do for now
+        // A list of the names of files in the latest release, useful if you have multiple builds (such as one for each support platform)
+        github_release_filenames: z.array(z.string()).optional(),
+
         other_url: z.string().url().optional(),
     }),
 });
